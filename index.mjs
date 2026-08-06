3d464e0b5b3659d5c50ee29e69af5bf05b4c0e1d
Kelvin Muterian <muteriank17@gmail.com>
2026-08-06 23:06:25 +0300

    Add index module to export town-related functions for improved accessibility

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import kenyaTowns from './index.js';
+
+export const {
+  getAll,
+  search,
+  getByName,
+  getByCounty,
+  getByConstituency,
+  getByType,
+  getSatelliteTowns,
+  nearBy,
+  listCounties,
+  query,
+} = kenyaTowns;
+
+export default kenyaTowns;
